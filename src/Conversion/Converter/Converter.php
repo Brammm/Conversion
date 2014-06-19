3b52cce896a6b2405dd39ff16ea133dfe0d804b8
Bram Van der Sype <bram.vandersype@gmail.com>
2014-06-19 20:12:56 +0200

    Cleaned up converter, added protection

--- src/Conversion/Converter/Converter.php
+++ src/Conversion/Converter/Converter.php
@@ -3,21 +3,48 @@
 namespace Conversion\Converter;
 
 use Conversion\Unit\UnitFactory;
+use Conversion\Unit\UnitInterface;
 
 class Converter
 {
 
+    /**
+     * Converts a value from one unit to another
+     *
+     * @param $value
+     * @param $fromUnit
+     * @param $toUnit
+     *
+     * @return float
+     */
     public function convert($value, $fromUnit, $toUnit)
     {
         $unitFrom = UnitFactory::getUnit($fromUnit);
         $unitTo   = UnitFactory::getUnit($toUnit);
 
-        // TODO: validate unitfrom and unitto belong to same category (weight, temp...)
+        $this->protectInvalidConversion($unitFrom, $unitTo);
 
-        $base = $unitFrom->toBase($value);
+        $base      = $unitFrom->toBase($value);
         $converted = $unitTo->fromBase($base);
 
         return $converted;
+    }
+
+    /**
+     * Checks if the conversion is valid
+     *
+     * @param UnitInterface $unitFrom
+     * @param UnitInterface $unitTo
+     */
+    public function protectInvalidConversion(UnitInterface $unitFrom, UnitInterface $unitTo)
+    {
+
+        $categoryFrom = explode('\\', get_class($unitFrom))[2];
+        $categoryTo   = explode('\\', get_class($unitTo))[2];
+
+        if ($categoryFrom !== $categoryTo) {
+            throw new \InvalidArgumentException(sprintf("Can't convert from %s to %s", $categoryFrom, $categoryTo));
+        }
     }
 
 } 
